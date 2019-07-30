728bbd714a078a61a3d560be04fbda4804a3d853
Сергей <46688253+Gendalif@users.noreply.github.com>
2019-07-30 22:34:22 +0300

    add some structure

--- src/main/resources/db/migration/coffe_houses.sql
+++ src/main/resources/db/migration/coffe_houses.sql
@@ -1,3 +1,3 @@
 create table coffe_houses (id integer not null auto_increment, advantages varchar(255),
-                           cuisine_type varchar(255), description varchar(2048), house_type varchar(255), location varchar(255),
+                           cuisine_type varchar(255), description varchar(255), house_type varchar(255), location varchar(255),
                            title varchar(255), working_hours integer, primary key (id)) engine=MyISAM
